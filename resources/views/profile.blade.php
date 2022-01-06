4ee5a7f28deab31c8e3fbbd781c052c8f5f800aa
Rakesh <rakeshben11@gmail.com>
2022-01-06 22:12:39 +0800

    test changes

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -26,21 +26,6 @@
     
     <div class="container-fluid mt--7">
       <div class="row">
-        {{-- <div class="col-xl-4 order-xl-2 mb-5 mb-xl-0">
-          <div class="card card-profile shadow">
-            <div class="row justify-content-center">
-              <div class="col-lg-3 order-lg-2">
-                
-              </div>
-            </div>
-            <div class="card-body pt-0 pt-md-4"style="padding-bottom=20px">
-              <div class="row">
-              </div>
-              <div class="text-center">
-              </div>
-            </div>
-          </div>
-        </div> --}}
         <div class="col-xl-8 order-xl-1">
           <div class="card bg-secondary shadow">
             <div class="card-header bg-white border-0">
